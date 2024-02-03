394c539f3cb7042deb076e346cdd8304e2632ca8
Tobiasz Fic <tobi.fic@gmail.com>
2024-02-03 23:15:21 +0100

    Delete static constexpr string to make CI work

--- test/src/util_string_tests.cpp
+++ test/src/util_string_tests.cpp
@@ -8,24 +8,24 @@ namespace
     // NOLINTBEGIN
     TEST(string_tests, longer_test)
     {
-        static constexpr std::string s1 = "helooo";
-        static constexpr std::string s2 = "haii";
+        std::string s1 = "helooo";
+        std::string s2 = "haii";
         EXPECT_EQ(s1, get_longer_string(s1, s2));
         EXPECT_EQ(s1, get_longer_string(s2, s1));
     }
 
     TEST(string_tests, shorter_test)
     {
-        static constexpr std::string s1 = "helooo";
-        static constexpr std::string s2 = "haii";
+        std::string s1 = "helooo";
+        std::string s2 = "haii";
         EXPECT_EQ(s2, get_shorter_string(s1, s2));
         EXPECT_EQ(s2, get_shorter_string(s2, s1));
     }
 
     TEST(string_tests, eq_test)
     {
-        static constexpr std::string s1 = "helo";
-        static constexpr std::string s2 = "haii";
+        std::string s1 = "helo";
+        std::string s2 = "haii";
         EXPECT_EQ(s1, get_longer_string(s1, s2));
         EXPECT_EQ(s2, get_longer_string(s2, s1));
         EXPECT_EQ(s2, get_shorter_string(s1, s2));
@@ -34,8 +34,8 @@ namespace
 
     TEST(string_tests, distance_test)
     {
-        static constexpr std::string s1 = "helo";
-        static constexpr std::string s2 = "hi";
+        std::string s1 = "helo";
+        std::string s2 = "hi";
         EXPECT_EQ(0, string_distance(s1, s1));
         EXPECT_EQ(0, string_distance(s2, s2));
         EXPECT_EQ(514, string_distance(s1, s2));
